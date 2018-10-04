b4baa943c9a2931b4327a919f09d67537c2c5f8e
Andrés Felipe Gamboa Torres <andresfdgt@outlook.com>
2018-10-04 11:01:22 -0500

    Uso de plantillas

--- Oansa/blank-page.1.php
+++ Oansa/blank-page.1.php
@@ -34,6 +34,14 @@
       </div>
     </main>
 
-  <?php
+    <!-- Essential javascripts for application to work-->
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+    <!-- The javascript plugin to display page loading on top-->
+    <script src="js/plugins/pace.min.js"></script>
+    <!-- Page specific javascripts-->
+    <?php
     include_once 'plantillas/fin.inc.php'
-  ?>
+    ?>
